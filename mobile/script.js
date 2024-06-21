79350858765a1c385ff048a061b5b8a08c4434ac
Treedeo <treedeo@yahoo.com>
2024-06-21 21:44:06 +0300

    Update script.js

--- mobile/script.js
+++ mobile/script.js
@@ -17,6 +17,11 @@ function adjustContainerSize() {
 
     container.style.width = `${width}px`;
     container.style.height = `${height}px`;
+
+    // Убедимся, что элементы внутри контейнера остаются в центре
+    const paginationContainer = document.querySelector('.pagination-container');
+    paginationContainer.style.width = `${width}px`;
+    paginationContainer.style.height = '40px';
 }
 
 // Изначальная настройка контейнера при загрузке страницы
