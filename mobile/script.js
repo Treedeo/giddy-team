607697df6b7fd5df2ad8dad11d1586e783c40aa6
Treedeo <treedeo@yahoo.com>
2024-06-22 06:15:21 +0300

    Update script.js

--- mobile/script.js
+++ mobile/script.js
@@ -7,17 +7,27 @@ const swiper = new Swiper('.swiper-container', {
     loop: true,
 });
 
-// Скрипт для адаптации контейнера в зависимости от размера экрана
-window.addEventListener('resize', adjustContainerSize);
+// Обработчики для изменения скидки
+const discountValue = document.querySelector('.discount-value');
+const leftArrow = document.querySelector('.arrow-left');
+const rightArrow = document.querySelector('.arrow-right');
+const buyButton = document.querySelector('.buy-button');
 
-function adjustContainerSize() {
-    const container = document.querySelector('.container');
-    const width = window.innerWidth;
-    const height = window.innerHeight;
+let discount = 5;
+leftArrow.addEventListener('click', () => {
+    if (discount > 5) {
+        discount -= 5;
+        updateDiscount();
+    }
+});
 
-    container.style.width = `${width}px`;
-    container.style.height = `${height}px`;
-}
+rightArrow.addEventListener('click', () => {
+    if (discount < 15) {
+        discount += 5;
+        updateDiscount();
+    }
+});
 
-// Изначальная настройка контейнера при загрузке страницы
-adjustContainerSize();
+function updateDiscount() {
+    discountValue.textContent = `${discount}%`;
+    buyButton.textContent = `${discount * 2
