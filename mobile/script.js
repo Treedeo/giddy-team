81efa9d0f4aa01286d88227c45811674225420f7
Treedeo <treedeo@yahoo.com>
2024-06-22 06:20:01 +0300

    Update script.js

--- mobile/script.js
+++ mobile/script.js
@@ -30,4 +30,20 @@ rightArrow.addEventListener('click', () => {
 
 function updateDiscount() {
     discountValue.textContent = `${discount}%`;
-    buyButton.textContent = `${discount * 2
+    buyButton.textContent = `${discount * 2} COM`;
+}
+
+// Скрипт для адаптации контейнера в зависимости от размера экрана
+window.addEventListener('resize', adjustContainerSize);
+
+function adjustContainerSize() {
+    const container = document.querySelector('.container');
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+
+    container.style.width = `${width}px`;
+    container.style.height = `${height}px`;
+}
+
+// Изначальная настройка контейнера при загрузке страницы
+adjustContainerSize();
